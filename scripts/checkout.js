be7da02d82c4764c687235885b6280bd181d1a22
zawarudo <skweex@gmail.com>
2024-11-25 14:29:07 +0200

    fixed price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,9 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 let cartSummaryHTML = '';
-cart.forEach(() => {
+
+cart.forEach((cartItem) => {
   const productId = cartItem.productId;
   let matchingProduct;
   products.forEach((product) => {
@@ -10,7 +12,8 @@ cart.forEach(() => {
     }
   });
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container 
+  js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -24,7 +27,7 @@ cart.forEach(() => {
           ${matchingProduct.name}
         </div>
         <div class="product-price">
-          $${matchingProduct.priceCents / 100}
+          $${formatCurrency(matchingProduct.priceCents)}
         </div>
         <div class="product-quantity">
           <span>
@@ -48,7 +51,7 @@ cart.forEach(() => {
         <div class="delivery-option">
           <input type="radio" checked
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Tuesday, June 21
@@ -61,7 +64,7 @@ cart.forEach(() => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Wednesday, June 15
@@ -74,7 +77,7 @@ cart.forEach(() => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Monday, June 13
