4251956401e13222830e8cdf26f3fc9303afad26
zawarudo <skweex@gmail.com>
2024-12-01 14:23:32 +0200

    Converted loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -89,6 +89,28 @@ const object3 = {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products')
+    .then((response) => {
+      return response.json();
+    })
+    .then((prodcutsData) => {
+      products = prodcutsData.map((productDetails) => {
+        if (productDetails.type === 'clothing') {
+          return new Clothing(productDetails);
+        }
+        return new Product(productDetails);
+      });
+      console.log('load prodcuts');
+    });
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
   xhr.addEventListener('load', () => {
