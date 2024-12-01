638685fee2c74c18b82eda5b16e4fe210b70369b
zawarudo <skweex@gmail.com>
2024-12-01 15:06:47 +0200

    Learned error handling

--- data/products.js
+++ data/products.js
@@ -102,9 +102,13 @@ export function loadProductsFetch() {
         return new Product(productDetails);
       });
       console.log('load prodcuts');
+    })
+    .catch((error) => {
+      console.log('Unexpected error. Please try again later.');
     });
   return promise;
 }
+
 /*
 loadProductsFetch().then(() => {
   console.log('next step');
@@ -123,6 +127,10 @@ export function loadProducts(fun) {
     console.log('load prodcuts');
     fun();
   });
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
